efdc4a287d5db3b8d1acc5258631a8c515e43059
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2018-10-06 23:16:51 +0900

    Add files via upload

---
+++ TCP_Listener/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace TCP_Listener
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.clientlen = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(12, 508);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(64, 15);
+            this.status.TabIndex = 1;
+            this.status.Text = "Status : ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 72);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 433);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "OPEN / HTML RESETUP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // clientlen
+            // 
+            this.clientlen.AutoSize = true;
+            this.clientlen.Location = new System.Drawing.Point(12, 523);
+            this.clientlen.Name = "clientlen";
+            this.clientlen.Size = new System.Drawing.Size(87, 15);
+            this.clientlen.TabIndex = 4;
+            this.clientlen.Text = "접속자 수 : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(583, 25);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "/";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(111, 25);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "127.0.0.1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(147, 41);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(52, 25);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "80";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = ":";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 547);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.clientlen);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label clientlen;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TCP_Listener/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TCP_Listener;
+
+namespace TCP_Listener
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public TcpListener tcp;
+
+        public int port;
+        public string[] richline;
+        public string richtext;
+        public int clientlength = 0;
+        public string path;
+
+        delegate void CrossThreadSafetySetText(Control ctl, String text);
+
+        private void CSafeSetText(Control ctl, String text)
+        {
+
+            /*
+            * InvokeRequired 속성 (Control.InvokeRequired, MSDN)
+            *   짧게 말해서, 이 컨트롤이 만들어진 스레드와 현재의 스레드가 달라서
+            *   컨트롤에서 스레드를 만들어야 하는지를 나타내는 속성입니다.  
+            * 
+            * InvokeRequired 속성의 값이 참이면, 컨트롤에서 스레드를 만들어 텍스트를 변경하고,
+            * 그렇지 않은 경우에는 그냥 변경해도 아무 오류가 없기 때문에 텍스트를 변경합니다.
+            */
+            if (ctl.InvokeRequired)
+                ctl.Invoke(new CrossThreadSafetySetText(CSafeSetText), ctl, text);
+            else
+                ctl.Text = text;
+        }
+
+        /*private void button1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.html = new System.Collections.Specialized.StringCollection();
+            foreach (string line in richTextBox1.Lines)
+            {
+                Properties.Settings.Default.html.Add(line);
+            }
+            Properties.Settings.Default.Save();
+
+            try
+            {
+                tcp.Stop();
+            }
+            catch (Exception ex) { }
+
+            Thread tcpserver = new Thread(tcp_server);
+            tcpserver.Start();
+
+            richline = richTextBox1.Lines;
+            richtext = richTextBox1.Text;
+            path = textBox1.Text;
+
+            status.Text = "Status : Server Open";
+        }*/
+
+        public void tcp_server()
+        {
+            tcp = new TcpListener(port);
+            tcp.Start();
+
+            try
+            {
+                while (true)
+                {
+                    TcpClient tc = tcp.AcceptTcpClient();
+
+                    NetworkStream cs = tc.GetStream();
+                    StreamReader cr = new StreamReader(cs);
+                    StreamWriter cw = new StreamWriter(cs);
+
+                    MessageBox.Show(cr.ReadLine());
+
+                    string cpath = cr.ReadLine().Split(new[] { "GET " }, StringSplitOptions.None)[1].Split(new[] { " HTTP" }, StringSplitOptions.None)[0];
+
+                    if (cpath == path)
+                    {
+                        clientlength += 1;
+
+                        CSafeSetText(status, "Status : Client Connected");
+                        CSafeSetText(clientlen, "접속자 수 : " + clientlength);
+
+                        cw.WriteLine("HTTP/1.1 200 OK");
+                        //cw.WriteLine("Content-Length: " + Encoding.UTF8.GetBytes(richtext).Length);
+                        //cw.WriteLine("Content-Length: " + Encoding.Default.GetByteCount(richtext));
+                        cw.WriteLine("Connection: close");
+                        cw.WriteLine("Content-Type: text/html; charset=utf-8");
+                        cw.WriteLine("");
+
+                        foreach (string line in richline)
+                        {
+                            cw.WriteLine(line);
+                        }
+                        cw.Flush();
+                    }
+                    else
+                    {
+                        /*cw.WriteLine("HTTP/1.1 404 Not Found");
+                        cw.WriteLine("");
+                        cw.Flush();*/
+
+                        cw.WriteLine("HTTP/1.1 200 OK");
+                        //cw.WriteLine("Content-Length: " + Encoding.Default.GetByteCount(richtext));
+                        cw.WriteLine("Connection: close");
+                        cw.WriteLine("Content-Type: text/html; charset=utf-8");
+                        cw.WriteLine("");
+                        cw.WriteLine("<html><body><h1>404 Not Found</h1><h2>HTTP ERROR 404</h2></body></html>");
+                        cw.Flush();
+                    }
+                }
+            }
+            catch(Exception ex) { }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Lines = Properties.Settings.Default.html.Cast<string>().ToArray();
+
+            textBox1.Text = Properties.Settings.Default.path;
+
+            textBox2.Text = WebGetText("https://ip.pe.kr/").Split(new[] { "<h1 class=\"cover-heading\">" }, StringSplitOptions.None)[1].Split(new[] { "</h1>" }, StringSplitOptions.None)[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.html = new System.Collections.Specialized.StringCollection();
+            foreach (string line in richTextBox1.Lines)
+            {
+                Properties.Settings.Default.html.Add(line);
+            }
+            Properties.Settings.Default.Save();
+
+            try
+            {
+                tcp.Stop();
+            }
+            catch (Exception) { }
+
+            clientlength = 0;
+            CSafeSetText(clientlen, "접속자 수 : " + clientlength);
+
+            port = int.Parse(textBox3.Text);
+
+            Thread tcpserver = new Thread(tcp_server);
+            tcpserver.Start();
+
+            richline = richTextBox1.Lines;
+            richtext = richTextBox1.Text;
+            path = textBox1.Text;
+
+            status.Text = "Status : Server Open";
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                tcp.Stop();
+            }
+            catch (Exception) { }
+
+            Properties.Settings.Default.html = new System.Collections.Specialized.StringCollection();
+            foreach (string line in richTextBox1.Lines)
+            {
+                Properties.Settings.Default.html.Add(line);
+            }
+
+            Properties.Settings.Default.path = textBox1.Text;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private string WebGetText(string url)
+        {
+            try
+            {
+                System.Net.WebRequest request = System.Net.WebRequest.Create(url);
+                System.Net.WebResponse response = request.GetResponse();
+
+                System.IO.StreamReader sr = new System.IO.StreamReader(response.GetResponseStream());
+
+                return sr.ReadToEnd();
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
